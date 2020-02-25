01eaabb57d5821e1324ba14e2d1948c7939b5d53
Maurizio Crocci <mbcrocci@gmail.com>
2020-02-25 14:17:05 +0000

    Increases version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Yeelight.MixProject do
     [
       app: :yeelight,
       licenses: "MIT License",
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.8",
       description: description(),
       name: "yeelight",
